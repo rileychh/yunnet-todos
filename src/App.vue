7f8011b96fab417f91ec53f727bfa0a821b87a16
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-11-05 15:07:24 +0800

    init again

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+      <h1>YunNet To-dos</h1>
+    </header>
+
+    <form class="card">
+      <h2>新增一個待辦事項</h2>
+      <label for="contents" style="display: none">內容</label>
+      <input id="contents" type="text" placeholder="內容..." />
+      <button type="submit">新增</button>
+    </form>
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/reset.css";
+import "./assets/main.css";
+
+createApp(App).mount("#app");
