633e3080c1c1e3290e348538ff69be81b4011e77
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-11-05 22:44:26 +0800

    Remove work; add guide comments

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,12 @@
+<!-- JavaScript -->
 <script setup>
-import { ref } from "vue";
-import Note from "./components/Note.vue";
-
-const notes = ref([]);
-const contents = ref("");
-
-function addNote() {
-  notes.value.push(contents.value);
-  contents.value = "";
-}
 </script>
 
+<!-- HTML -->
 <template>
-  <header>
-    <h1>YunNet To-dos</h1>
-  </header>
-
-  <form class="card">
-    <h2>新增一個待辦事項</h2>
-    <input v-model="contents" id="contents" type="text" placeholder="內容..." />
-    <button type="submit" @click.prevent="addNote">新增</button>
-  </form>
-
-  <main>
-    <Note v-for="note in notes" :contents="note" />
-  </main>
+  <p>Hello, world!</p>
 </template>
 
+<!-- CSS -->
 <style scoped>
-header {
-  background-color: lightgray;
-  padding: 1rem;
-}
-
-h1 {
-  font-size: 1.5rem;
-  font-weight: bold;
-}
-
-h2 {
-  font-size: 1.25rem;
-  font-weight: bold;
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.5rem;
-  width: min-content;
-  margin: 0.5rem auto;
-  padding: 1rem;
-  border: 1px lightgray solid;
-  border-radius: 0.25rem;
-}
-
-.card button {
-  all: unset;
-  cursor: pointer;
-  color: #00aaff;
-  align-self: flex-end; /* align to right */
-}
-
-main {
-  display: flex;
-  gap: 0.5rem;
-  justify-content: center;
-  flex-wrap: wrap;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "./assets/reset.css";
+// import "./assets/reset.css";
 
 createApp(App).mount("#app");
