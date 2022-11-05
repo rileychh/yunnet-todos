7a20bdad10a6b2f16ba408e66a24ff9a1d8c3d7a
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-11-05 15:54:30 +0800

    minimal example completed

---
+++ src/components/Note.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["contents"]);
+</script>
+
+<template>
+  <article>
+    <p>{{ props.contents }}</p>
+  </article>
+</template>
+
+<style scoped>
+  article {
+    width: 10rem;
+    height: 10rem;
+    background-color: #F3E779;
+    padding: 1rem;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/reset.css";
-import "./assets/main.css";
 
 createApp(App).mount("#app");
